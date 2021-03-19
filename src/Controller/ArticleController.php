15c08cde8912c65c1245084e2c5f8e43fa9e6a0a
Hugo Dauphin <hugodauphin33@gmail.com>
2021-03-19 12:19:13 +0100

    premier commi

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Controller;
+
+//je donne les namespaces dont j'aurais besoin pour ma class
+
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+// Je créé ma class je extend avec AbstractController pour bénéficier des fonctions de celui-ci
+
+class ArticleController extends AbstractController
+{
+
+    // Je créé ma route, l'url à appeler pour lancer ma fonction
+
+    /**
+     * @Route("/articles", name="articles")
+     */
+    public function listArticles(ArticleRepository $articleRepository)
+    {
+        // faire une requête en base de données
+        // pour récupérer tous les articles de la table article.
+
+        $articles = $articleRepository->findAll();
+
+        // Je retourne le rendu à mon twig Articles qui est ma liste d'articles
+
+        return $this->render('articles.html.twig', [
+            'articles' => $articles
+        ]);
+    }
+
+    //Comme précédement je créé ma route mais j'y ajoute une whitecard
+    //Afin d'avoir un contenu ciblé
+    /**
+     * @Route("/articles/{id}", name="show_article")
+     */
+
+    //Dans ma fonction je donne le parametre de ma whitecard
+    //J'appelle ArticleRepository pour pouvoir faire des requetes vers ma BDD
+
+    public function showArticle(ArticleRepository $articleRepository ,$id){
+
+        // Dans ma requete je cherche l'article qui à l'id correspondant à la whitecard
+        $article = $articleRepository->findOneBy(['id'=>$id]);
+
+        // Je retourne le rendu à mon twig Articles qui est ma liste d'articles
+        return $this->render('article.html.twig', [
+                'article'=>$article]);
+    }
+
+
+    //Je met ma route qui ira vers ma page d'acceuil
+    /**
+     * @Route("/home", name="home")
+     */
+
+    public function homePage(ArticleRepository $articleRepository){
+
+        // Je fais une requete avant de selectionner les deux derniers articles sortis
+        $article = $articleRepository->findBy(
+            ["published"=> '1'],['id'=>"DESC"],2
+        );
+
+        //je retourne le résultat  a ma vue twig home
+        return $this->render('home.html.twig',[
+            'articles'=>$article
+        ]);
+    }
+
+}
+
+
---
+++ src/Entity/Article.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $resume;
+
+    /**
+     * @ORM\Column(type="string", length=5000, nullable=true)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $published;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getResume(): ?string
+    {
+        return $this->resume;
+    }
+
+    public function setResume(?string $content): self
+    {
+        $this->resume = $resume;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getPublished(): ?bool
+    {
+        return $this->published;
+    }
+
+    public function setPublished(bool $published): self
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+}
